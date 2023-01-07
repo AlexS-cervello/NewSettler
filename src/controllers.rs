f45ae380e8b7d093fb921f7c2620bd94b15026ef
Feston229 <londonter@protonmail.com>
2023-01-07 21:43:17 +0530

    feat(): habr

--- src/controllers.rs
+++ src/controllers.rs
@@ -10,12 +10,23 @@ use teloxide::{requests::Requester, types::ChatId, Bot};
 pub async fn start_pooling(db: &Database, bot: Bot) {
     let mut last_new_hackernew = String::new();
     let mut last_new_habr = String::new();
+    let mut count: u16 = 1;
     loop {
-        // Every minute checks for hacker new
+        // Every minute checks for news
+        let timer = tokio::spawn(tokio::time::sleep(Duration::from_secs(60)));
+
         send_last_new_hackernews(db, &bot, &mut last_new_hackernew)
             .await
             .unwrap_or_else(|err| log::error!("{}", err));
-        tokio::time::sleep(Duration::from_secs(60)).await;
+
+        if count % 60 == 0 {
+            send_last_new_habr(db, &bot, &mut last_new_habr)
+                .await
+                .unwrap_or_else(|err| log::error!("{}", err));
+            count = 0;
+        }
+        count += 1;
+        timer.await.unwrap();
     }
 }
 
@@ -49,21 +60,33 @@ async fn send_last_new_hackernews(
     bot: &Bot,
     last_new: &mut String,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let first_new = get_one_new_hackernews().await.unwrap_or_else(|err| {
-        log::error!("Error getting one new from hackernews: {}", err);
-        "Error getting new from hackernews".to_string()
-    });
-
+    let first_new = get_one_new_hackernews().await?;
     if first_new.as_str() != last_new {
         *last_new = first_new.clone();
         let user_ids = db.get_users_id().await;
         if let Ok(user_list) = user_ids {
             for user_id in user_list {
-                if let Err(err) =
-                    bot.send_message(ChatId(user_id), &first_new).await
-                {
-                    log::error!("{}", err)
-                }
+                bot.send_message(ChatId(user_id), &first_new).await?;
+            }
+        } else if let Err(err) = user_ids {
+            log::error!("{}", err)
+        }
+    }
+    Ok(())
+}
+
+async fn send_last_new_habr(
+    db: &Database,
+    bot: &Bot,
+    last_new: &mut String,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let first_new = get_one_new_habr().await?;
+    if first_new.as_str() != last_new {
+        *last_new = first_new.clone();
+        let user_ids = db.get_users_id().await;
+        if let Ok(user_list) = user_ids {
+            for user_id in user_list {
+                bot.send_message(ChatId(user_id), &first_new).await?;
             }
         } else if let Err(err) = user_ids {
             log::error!("{}", err)
--- src/parsing_data.rs
+++ src/parsing_data.rs
@@ -4,23 +4,24 @@ const URL_HACKN: &'static str = "https://news.ycombinator.com/";
 const SEL_HACKN: &'static str = "span.titleline>a";
 const URL_HABR: &'static str = "https://habr.com";
 const SEL_HABR_HREF: &'static str = "article>div>h2>a";
+const SEL_HABR_TITLE: &'static str = "article>div>h2>a>span";
 
 async fn get_parsing_vars(
     url: &str,
     selector: &Vec<&'static str>,
-) -> Result<(Html, Vec<Selector>), Box<dyn std::error::Error>> {
+) -> Result<(Html, Vec<Selector>), Box<dyn std::error::Error + Send + Sync>> {
     let response = reqwest::get(url).await?.text().await?;
     let document = Html::parse_document(&response);
     let selectors = selector
         .into_iter()
-        .map(|el| Selector::parse(el))
-        .collect::<Result<Vec<Selector>, _>>()?; //TODO replace that underscore
+        .map(|el| Selector::parse(el).unwrap()) // TODO Replace that unwrap
+        .collect::<Vec<Selector>>();
 
     Ok((document, selectors))
 }
 
 pub async fn parse_starting_news(
-) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+) -> Result<Vec<String>, Box<dyn std::error::Error + Send + Sync>> {
     let mut result: Vec<String> = vec![];
     let (document, selector) =
         get_parsing_vars(URL_HACKN, &vec![SEL_HACKN]).await?;
@@ -36,7 +37,8 @@ pub async fn parse_starting_news(
         })
         .zip(0..10)
         .for_each(|(val, _)| {
-            if &val[0..7] != "Ask HN:" {
+            let compare = &val.replace(|c: char| !c.is_ascii(), "");
+            if compare != "Ask HN:" {
                 result.push(val);
             }
         });
@@ -44,7 +46,7 @@ pub async fn parse_starting_news(
 }
 
 pub async fn get_one_new_hackernews(
-) -> Result<String, Box<dyn std::error::Error>> {
+) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
     let (document, selector) =
         get_parsing_vars(URL_HACKN, &vec![SEL_HACKN]).await?;
     let result = document
@@ -59,19 +61,34 @@ pub async fn get_one_new_hackernews(
         .next()
         .unwrap_or("".to_string());
     // Exception - no data here
-    if &result[0..7] == "Ask HN:" {
+    let compare = &result.replace(|c: char| !c.is_ascii(), "");
+    if compare == "Ask HN:" {
         return Ok("".to_string());
     }
     Ok(result)
 }
 
-pub async fn get_one_new_habr() -> Result<String, Box<dyn std::error::Error>> {
+pub async fn get_one_new_habr(
+) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
     let (document, selector) =
-        get_parsing_vars(URL_HABR, &vec![SEL_HABR_HREF]).await?;
-    let result = document
+        get_parsing_vars(URL_HABR, &vec![SEL_HABR_TITLE, SEL_HABR_HREF])
+            .await?;
+    let title = document
         .select(&selector[0])
         .map(|el| format!("{}", el.inner_html()))
         .next()
         .unwrap_or("".to_string());
+    let href = document
+        .select(&selector[1])
+        .map(|el| {
+            format!(
+                "{}{}",
+                URL_HABR,
+                el.value().attr("href").unwrap_or("").to_string()
+            )
+        })
+        .next()
+        .unwrap_or("".to_string());
+    let result = format!("{}\n{}", title, href);
     Ok(result)
 }
